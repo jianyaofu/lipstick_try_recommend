b5b321ad80c88c58fd0cd2b2747481e9e0dac56b
Rileyyz <44385284+Rileyyz@users.noreply.github.com>
2018-12-02 15:07:10 -0500

    lipstick_text_mining

--- lipstick_text_mining.py
+++ lipstick_text_mining.py
@@ -7,7 +7,6 @@ from scipy.misc import imread
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 from nltk.corpus import stopwords
 
-
 def search_link(brand_name: int, product_name: int) -> str:
     df = pd.read_csv('lipstick.csv')
     df = df.drop(['Unnamed: 0'],axis = 1)
@@ -16,14 +15,15 @@ def search_link(brand_name: int, product_name: int) -> str:
     link = search_df['link'].iloc[0]
     return link
 
-def get_wordcloud(reviews):
+def get_wordcloud(outcome):
     texts = []
-    [texts.append(review) for review in reviews]
+    [texts.append(review) for review in outcome]
+    # outcome['reviews']
     text_string = ''
     text_string_2 = ''
 
     #Remove unwanted words
-    DELETE_WORDS = ['lipstick', 'lip', 'idea', 'product', 'the','think', 'found','feel', 'lunch','even', 'really']
+    DELETE_WORDS = ['lipstick', 'lip', 'idea', 'product', 'the','think', 'found','feel', 'lunch','even', 'really','color','look']
     def remove_words(text_string,DELETE_WORDS=DELETE_WORDS):
         for word in DELETE_WORDS:
             text_string = text_string.replace(word,' ')
@@ -38,26 +38,13 @@ def get_wordcloud(reviews):
                 text_string = text_string.replace(' '+word+' ',' ',1)
         return text_string
 
-
-
-    for i in texts:
-        string = remove_words(i)
-        string = remove_short_words(string)
-        text_string = text_string + string
-    words = word_tokenize(text_string)
-    text_string = [word.lower() for word in words
-                      if word not in stopwords.words() and word.isalpha()]
+    text_string_2 = [text_string + remove_short_words(remove_words(i)) for i in texts]
 
     text_string_ = ''
-    for i in text_string:
-        text_attr = [i]
-        tag = nltk.pos_tag(text_attr)
-        if 'RB' in tag[0][1] or 'NN' in tag[0][1] or 'JJ' in tag[0][1]:
-            text_string_ = text_string_ + ' ' + i
+    text_string_ = [text_string_ + ' ' + i for i in text_string_2 if ('RB' in nltk.pos_tag([i])[0][1]) or ('NN' in nltk.pos_tag([i])[0][1]) or ('JJ' in nltk.pos_tag([i])[0][1])]
+    text_string_
     bg_pic = imread('lip.jpg')
-    
-    wordcloud = WordCloud(stopwords=STOPWORDS,mask=bg_pic, background_color='white',width=1200,height=1500,max_words = 50).generate(text_string_)
-    name = 'word_cloud.jpg'
+    wordcloud = WordCloud(stopwords=STOPWORDS, mask=bg_pic, background_color='white',width=1200,height=1500,max_words = 50).generate(str(text_string_))
+    name ='word_cloud.jpg'
     image_colors = ImageColorGenerator(bg_pic)
-
     WordCloud.to_file(wordcloud,name)
